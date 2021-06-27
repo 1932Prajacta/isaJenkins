b75f3318e34cb06982c38be75eb225033c8c5d2c
1932Prajacta <mca.1932@unigoa.ac.in>
2021-06-27 05:30:06 -0700

    test:

---
+++ TestLogging.java
@@ -0,0 +1,32 @@
+package testLogging;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TestLogging {
+	@Given("I want to enter valid name1 and pass1")
+	public void i_want_to_enter_valid_name1_and_pass1() {
+	    // Write code here that turns the phrase above into concrete actions
+	    System.out.println("Inside : I want to enter valid name1 and pass1");
+	}
+
+	@When("I click on login button")
+	public void i_click_on_login_button() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("Inside : I click on login button");
+	}
+
+	@Then("I should be able to login")
+	public void i_should_be_able_to_login() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("Inside : I should be able to login");
+	}
+
+	@Given("I want to enter valid name2 and pass2")
+	public void i_want_to_enter_valid_name2_and_pass2() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("Inside : I want to enter valid name2 and pass2");
+	}
+
+}
