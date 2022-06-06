d1f03f49edd022de201b9992b13cb33f8f1dab8e
Prajacta <mca.1932@unigoa.ac.in>
2022-06-06 14:58:04 +0530

    changes done by the GitXguy just to validate branching

--- cucumber-Project/src/test/java/testLogging/DemoClass.java
+++ cucumber-Project/src/test/java/testLogging/DemoClass.java
@@ -4,5 +4,10 @@ public class DemoClass {
 
 	public static void main(String [] args) {
 		System.out.println("Demo class file for understanding version control system");
+		System.out.println("This is a dummy file");
+	}
+	
+	public void addMethod() {
+		System.out.println("perform addition");
 	}
 }
